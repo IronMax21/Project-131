b56f8bae1ecd22b701b7e9f83879229c61390e96
IronMax21 <91037892+IronMax21@users.noreply.github.com>
2023-03-01 21:26:04 -0500

    Add files via upload

---
+++ object-detection/main.js
@@ -0,0 +1,26 @@
+/*img = "";
+//called before loading
+function preload()
+{
+    img = loadImage('dog_cat.jpg');
+}
+//used to create canvas
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+}
+//executed in every frame
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+    fill("red");
+    text("dog", 100, 100);
+    noFill();
+    stroke("blue");
+    rect(50, 50, 300, 350);
+    text("cat", 400, 100);
+    noFill();
+    stroke("blue");
+    rect(300, 80, 300, 300);
+}*/
